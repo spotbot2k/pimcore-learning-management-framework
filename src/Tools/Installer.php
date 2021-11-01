b3c7d337538c87a100dd871fd56bea9ef1196444
Alexander Schwirjow <spotbot2k@yahoo.de>
2021-11-01 13:43:19 +0000

    Add basic data objects

--- src/Tools/Installer.php
+++ src/Tools/Installer.php
@@ -3,13 +3,18 @@
 namespace LearningManagementFrameworkBundle\Tools;
 
 use Pimcore\Logger;
+use Pimcore\Extension\Bundle\Installer\Exception\InstallationException;
 use Pimcore\Extension\Bundle\Installer\SettingsStoreAwareInstaller;
+use Pimcore\Model\DataObject\ClassDefinition;
+use Pimcore\Model\DataObject\ClassDefinition\Service;
+use Pimcore\Model\DataObject\Fieldcollection;
 
 class Installer extends SettingsStoreAwareInstaller
 {
     public function install()
     {
         $this->installClasses();
+        $this->installFieldCollections();
 
         parent::install();
 
@@ -25,17 +30,43 @@ class Installer extends SettingsStoreAwareInstaller
 
     public static function installClass($classname, $filepath)
     {
-        $class = \Pimcore\Model\DataObject\ClassDefinition::getByName($classname);
+        $class = ClassDefinition::getByName($classname);
         if (!$class) {
-            $class = new \Pimcore\Model\DataObject\ClassDefinition();
+            $class = new ClassDefinition();
             $class->setName($classname);
             $class->setGroup('CustomerManagement');
 
             $json = file_get_contents($filepath);
 
-            $success = \Pimcore\Model\DataObject\ClassDefinition\Service::importClassDefinitionFromJson($class, $json);
+            $success = Service::importClassDefinitionFromJson($class, $json);
             if (!$success) {
                 Logger::err("Could not import $classname Class.");
+            }
+        }
+    }
+
+    public function installFieldCollections()
+    {
+        $sourcePath = __DIR__.'/../Resources/install/fieldcollection_source';
+        $collections = ['Question'];
+
+        foreach ($collections as $key) {
+            self::installFieldCollection($key, $sourcePath);
+        }
+    }
+
+    public static function installFieldCollection($key, $filepath)
+    {
+        $fieldCollection = Fieldcollection\Definition::getByKey($key);
+        if (!$fieldCollection) {
+            $fieldCollection = new Fieldcollection\Definition();
+            $fieldCollection->setKey($key);
+
+            $data = file_get_contents(sprintf("%s/fieldcollection_%s_export.json", $filepath, $key));
+            $success = Service::importFieldCollectionFromJson($fieldCollection, $data);
+
+            if (!$success) {
+                throw new InstallationException(sprintf('Failed to import field collection "%s"', $key));
             }
         }
     }
