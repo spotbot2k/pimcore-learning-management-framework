fd85c50bac0a0313b4d257573818f61feff541a6
spotbot2k <spotbot2k@users.noreply.github.com>
2021-11-04 14:26:23 +0000

    Apply php-cs-fixer changes

--- src/DependencyInjection/PimcoreLearningManagementFrameworkExtension.php
+++ src/DependencyInjection/PimcoreLearningManagementFrameworkExtension.php
@@ -1,15 +1,9 @@
 <?php
 
 /**
- * Pimcore
+ * This file is a part of the Pimcore Learning Management Framework
+ * Docs and updates: https://github.com/spotbot2k/pimcore-learning-management-framework
  *
- * This source file is available under two different licenses:
- * - GNU General Public License version 3 (GPLv3)
- * - Pimcore Commercial License (PCL)
- * Full copyright and license information is available in
- * LICENSE.md which is distributed with this source code.
- *
- *  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
  *  @license    http://www.pimcore.org/license     GPLv3 and PCL
  */
 
