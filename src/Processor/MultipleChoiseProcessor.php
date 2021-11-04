448611dd0c657f2af46d121c7fb60b51e5a5a0a8
spotbot2k <spotbot2k@users.noreply.github.com>
2021-11-04 16:19:12 +0000

    Apply php-cs-fixer changes

--- src/Processor/MultipleChoiseProcessor.php
+++ src/Processor/MultipleChoiseProcessor.php
@@ -1,5 +1,12 @@
 <?php
 
+/**
+ * This file is part of the Pimcore Learning Management Framework
+ * Docs and updates: https://github.com/spotbot2k/pimcore-learning-management-framework
+ *
+ *  @license GPLv3
+ */
+
 namespace LearningManagementFrameworkBundle\Processor;
 
 class MultipleChoiseProcessor implements QuestionProcessorInterface
