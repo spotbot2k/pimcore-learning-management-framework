d684b42080e1e5a985ca9015c96c269cc7774eb3
spotbot2k <spotbot2k@users.noreply.github.com>
2021-11-16 18:09:17 +0000

    Apply php-cs-fixer changes

--- src/Result/ValidationResult.php
+++ src/Result/ValidationResult.php
@@ -1,5 +1,12 @@
 <?php
 
+/**
+ * This file is part of the Pimcore Learning Management Framework
+ * Docs and updates: https://github.com/spotbot2k/pimcore-learning-management-framework
+ *
+ *  @license GPLv3
+ */
+
 namespace LearningManagementFrameworkBundle\Result;
 
 class ValidationResult
